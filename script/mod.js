8f1ce4f6205959e0cffbf0cdf2e44e4a200835f8
Varzzz123 <varzgeteamabar@gmail.com>
2025-05-15 23:36:18 -0700

    GHG

--- script/mod.js
+++ script/mod.js
@@ -8,3 +8,21 @@ function Home() {
         loading.style.display = "none";
     }, 1000)
 }
+
+function andro() {
+    loading.style.display = "block";
+    
+    setTimeout(() => {
+        window.open("../HTML/MOD/ANDROID/halamanMod.html");
+        loading.style.display = "none";
+    }, 3000);
+}
+
+function dekstop() {
+    loading.style.display = "block";
+    
+    setTimeout(() => {
+        window.open("../HTML/MOD/DEKSTOP/halamanMod.html");
+        loading.style.display = "none";
+    }, 3000);
+}
